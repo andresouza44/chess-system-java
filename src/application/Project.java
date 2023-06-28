a6f35996e5eb19f653d4f205a9ea9ecbaf0afb0c
Andre M. de Souza <andresouza44@gmail.com>
2023-06-28 17:55:15 -0300

    arrumando pastas

---
+++ src/application/Project.java
@@ -0,0 +1,11 @@
+package application;
+
+import boardgame.Position;
+
+public class Project {
+    public static void main(String[] args) {
+        System.out.println("Teste");
+        Position position = new Position(5,10);
+        System.out.println(position);
+    }
+}
