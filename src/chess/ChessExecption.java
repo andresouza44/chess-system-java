c69d085d7b59a1f4e2040bfba47bca0dbcf45007
Andre M. de Souza <andresouza44@gmail.com>
2023-06-29 20:48:08 -0300

    chessExeption

---
+++ src/chess/ChessExecption.java
@@ -0,0 +1,7 @@
+package chess;
+
+public class ChessExecption extends RuntimeException{
+    public ChessExecption(String message) {
+        super(message);
+    }
+}
---
+++ src/chess/ChessPosition.java
@@ -0,0 +1,43 @@
+package chess;
+
+import boardgame.Position;
+
+import java.awt.*;
+
+public class ChessPosition {
+    private char column;
+    private int row;
+
+    public ChessPosition() {
+    }
+
+    public ChessPosition(char column, int row) {
+        if (column < 'a' || column > 'h' || row < 0 || row > 8) {
+            throw new ChessExecption("Error instantiating ChessPositon.Valid values are from a1 to h8 ");
+        }
+        this.column = column;
+        this.row = row;
+    }
+
+    public char getColumn() {
+        return column;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    protected Position toPosition() {
+        return new Position(8 - row, column - 'a');
+    }
+
+    protected static ChessPosition fromPosition(Position position) {
+        return new ChessPosition((char) ('a' - position.getColumn()), 8 - position.getRow());
+    }
+
+    @Override
+    public String toString() {
+        return "" + column + row;
+    }
+}
+
