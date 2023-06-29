3145daf2614e5fabfc818f3a6bc94b37f97b667e
Andre M. de Souza <andresouza44@gmail.com>
2023-06-28 21:21:57 -0300

    Criada a classs Piece e class Board

---
+++ src/application/ProjectTeste.java
@@ -0,0 +1,13 @@
+package application;
+
+import boardgame.Board;
+import boardgame.Position;
+
+public class ProjectTeste {
+    public static void main(String[] args) {
+        Board board = new Board(8,8);
+        
+
+    }
+
+}
