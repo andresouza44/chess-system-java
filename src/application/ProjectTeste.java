64bd54e77255348a05c79e18fefbfc3710b6df19
Andre M. de Souza <andresouza44@gmail.com>
2023-07-04 16:31:55 -0300

    jogada especial roque.

--- src/application/ProjectTeste.java
+++ src/application/ProjectTeste.java
@@ -4,10 +4,10 @@ import boardgame.Board;
 import boardgame.Position;
 
 public class ProjectTeste {
-    public static void main(String[] args) {
-        Board board = new Board(8,8);
+    public static void main(String[] args){
+        System.out.print("Everything on the console will be cleared");
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
 
-
-    }
-
+}
 }
