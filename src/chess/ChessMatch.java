b6c8e128b975e1966aa72867b3eae8fddb53b978
Andre M. de Souza <andresouza44@gmail.com>
2023-07-04 20:10:32 -0300

    Melhorias na jogada promoção.

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -5,12 +5,10 @@ import boardgame.Piece;
 import boardgame.Position;
 import chesspiece.*;
 
-import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static java.util.stream.Collectors.toList;
 
 public class ChessMatch {
     private int turn;
@@ -119,7 +117,7 @@ public class ChessMatch {
             throw new IllegalStateException("There is no piece to be promoted");
         }
         if (!type.equals("B") && !type.equals("N") && !type.equals("R") & !type.equals("Q")) {
-            throw new InvalidParameterException("Invalid type for promotion");
+           return promoted;
         }
 
         Position pos = promoted.getChessPosition().toPosition();
